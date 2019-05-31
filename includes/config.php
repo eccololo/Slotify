af85cf7bda907cd39a8dbd1a87c0cdec74185ac9
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2019-05-31 16:38:57 +0200

    create config.php, set connection with database

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+    //Wlacza output buffer ktory ulatwia przesylanie danych na serwer.
+    ob_start();
+
+    //Ustawiamy domyslny timezone tak zebysmy mieli poprawne daty i czas w bazie danych wstawiany
+    $timezone = date_default_timezone_set("Europe/Warsaw");
+
+    $con = mysqli_connect("localhost", "root", "root", "slotify");
+    if(mysqli_connect_errno()) {
+        echo "Failed to connect: " . mysqli_connect_errno();
+    }
+?>
