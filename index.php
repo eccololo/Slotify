c8b36ff6fe7c13641823cd6e50295c637281c51c
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2019-06-11 16:48:53 +0200

    add progress bar to index.php

--- index.php
+++ index.php
@@ -43,6 +43,11 @@
                                         <img src="assets/images/icons/repeat.png" alt="Repeat">
                                    </button>
                               </div>
+                              <div id="playbackBar">
+                                   <span class="progressTime current">0.00</span>
+                                   <div class="progressBar"></div>
+                                   <span class="progressTime remaining">0.00</span>
+                              </div>
                          </div>
                     </div>
                     <div id="nowPlayingRight">
