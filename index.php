4ae1d8d4db9ab25f6b92ccd44fac50692b779b5d
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2019-06-11 18:08:37 +0200

    add folume bar to playing bar

--- index.php
+++ index.php
@@ -72,7 +72,18 @@
                          </div>
                     </div>
                     <div id="nowPlayingRight">
-                         
+                         <div class="volumeBar">
+                              <button class="controlButton volume" title="Volume button">
+                                   <img src="assets/images/icons/volume.png" alt="Volume">
+                              </button>
+                              <div class="progressBar">
+                                        <div class="progressBarBg">
+                                             <div class="progress">
+
+                                             </div>
+                                        </div>
+                              </div>
+                         </div>
                     </div>
                </div>
          </div>
