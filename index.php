db23e823001c4eaebc151dc1a3155141bf89d8cc
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2019-07-09 13:43:01 +0200

    work on main content

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php include("includes/header.php"); ?>
 <h1 class="pageHeadingBig">You Might Also Like</h1>
+<div id="gridViewContainer">
+     <?php
+          $albumQuery = mysqli_query($con, "SELECT * FROM albums");
+
+          while($row = mysqli_fetch_array($albumQuery)) {
+               echo $row["title"];
+          }
+     ?>
+</div>
 <?php include("includes/footer.php"); ?>
 
              
