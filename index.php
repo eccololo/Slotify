f5863384d3f10c539b35e46f248fcc4f600243d7
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2019-07-09 14:00:12 +0200

    inserting artworks into main content div

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 <h1 class="pageHeadingBig">You Might Also Like</h1>
 <div id="gridViewContainer">
      <?php
-          $albumQuery = mysqli_query($con, "SELECT * FROM albums");
+          $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
 
           while($row = mysqli_fetch_array($albumQuery)) {
-               echo $row["title"];
+               echo "<div class='gridViewItem'>
+                         <img src='" . $row['artworkPath'] . "' class='gridViewArtwork'>
+                         <div class='gridViewInfo'>
+                              " . $row['title'] . "
+                         </div>
+                    </div>";
           }
      ?>
 </div>
