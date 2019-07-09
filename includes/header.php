4de42a86c26d6d33d425a2a114b5da79bb325cad
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2019-07-09 13:31:54 +0200

    refactoring code, adding separate files

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<?php
+     include("includes/config.php");
+
+     //session_destroy(); //LOGOUT
+
+     if(isset($_SESSION['userLoggedIn'])) {
+          $userLoggedIn = $_SESSION['userLoggedIn'];
+     } else {
+          header("Location: register.php");
+     }
+?>
+
+<html>
+
+<head>
+     <title></title>
+     <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+     <div id="mainContainer">
+          <div id="topContainer">
+               <?php include("includes/navBarContainer.php"); ?>
+
+               <div id="mainViewContainer">
+                    <div id="mainContent">
