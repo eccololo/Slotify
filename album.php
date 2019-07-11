ceb39d54ee163ce5acaf08221db54b82198cf7fb
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2019-07-11 16:39:39 +0200

    work on album.php, create Song.php class

--- album.php
+++ album.php
@@ -5,17 +5,19 @@
      } else {
          header("Location: index.php");
      }
-
-     $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
-     $album = mysqli_fetch_array($albumQuery);
-
-     $artist = new Artist($con, $album['artist']);
-
-     echo $album['title'];
-     echo "<br>";
-     echo $artist->getName();
-
-
+     $album = new Album($con, $albumId);
+     $artist = $album->getArtist();
 ?>
+
+<div class="entityInfo">
+     <div class="leftSection">
+        <img src="<?php echo $album->getArtworkPath(); ?>" alt="">
+     </div>
+     <div class="rightSection">
+        <h2><?php echo $album->getTitle(); ?></h2>
+        <p>By <?php echo $artist->getName(); ?></p>
+        <p><?php echo $album->getNumberOfSongs(); ?> songs</p>
+     </div>
+</div>
 
 <?php include("includes/footer.php"); ?>
---
+++ includes/classes/Album.php
@@ -0,0 +1,46 @@
+<?php
+class Album {
+
+    private $id;
+    private $con;
+    private $title;
+    private $artistId;
+    private $genre;
+    private $artworkPath;
+
+    public function __construct($con, $id) {
+        $this->id = $id;
+        $this->con = $con;
+
+        $albumQuery = mysqli_query($this->con, "SELECT * FROM albums WHERE id='$this->id'");
+        $album = mysqli_fetch_array($albumQuery);
+
+        $this->title = $album['title'];
+        $this->artistId = $album['artist'];
+        $this->genre = $album['genre'];
+        $this->artworkPath = $album['artworkPath'];
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getArtworkPath() {
+        return $this->artworkPath;
+    }
+
+    public function getArtist() {
+        return new Artist($this->con, $this->artistId);
+    }
+
+    public function getGenre() {
+        return $this->genre;
+    }
+
+    public function getNumberOfSongs() {
+        $query = mysqli_query($this->con, "SELECT id FROM songs WHERE album='$this->id'");
+        return mysqli_num_rows($query);
+    }
+        
+}
+?>
