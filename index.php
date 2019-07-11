856973f166bd823932edc237a3a75d78a200e328
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2019-07-11 08:47:08 +0200

    styling index.php artworks, add album.php page

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
 
           while($row = mysqli_fetch_array($albumQuery)) {
                echo "<div class='gridViewItem'>
-                         <img src='" . $row['artworkPath'] . "' class='gridViewArtwork'>
-                         <div class='gridViewInfo'>
-                              " . $row['title'] . "
-                         </div>
+                         <a href='album.php?id=" . $row['id'] . "'>
+                             <img src='" . $row['artworkPath'] . "' class='gridViewArtwork'>
+                             <div class='gridViewInfo'>
+                                  " . $row['title'] . "
+                             </div>
+                         </a>
                     </div>";
           }
      ?>
 </div>
 <?php include("includes/footer.php"); ?>
-
-             
