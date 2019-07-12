5f6bd29809371b9aa647b8c396051a389787b280
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2019-07-12 07:18:11 +0200

    work on Song.php class

--- includes/classes/Song.php
+++ includes/classes/Song.php
@@ -15,8 +15,43 @@ class Song {
         $this->id = $id;
         $this->con = $con;
 
-        $albumQuery = mysqli_query($this->con, "SELECT * FROM albums WHERE id='$this->id'");
-        $album = mysqli_fetch_array($albumQuery);
+        $songQuery = mysqli_query($this->con, "SELECT * FROM songs WHERE id='$this->id'");
+        $this->mysqliData = mysqli_fetch_array($songQuery);
+
+        $this->title = $this->mysqliData['title'];
+        $this->artistId = $this->mysqliData['artist'];
+        $this->albumId = $this->mysqliData['album'];
+        $this->genre = $this->mysqliData['genre'];
+        $this->duration = $this->mysqliData['duration'];
+        $this->path = $this->mysqliData['path'];
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getArtist() {
+        return new Artist($this->con, $this->artistId);
+    }
+
+    public function getAlbum() {
+        return new Album($this->con, $this->albumId);
+    }
+    
+    public function getPath() {
+        return $this->path;
+    }
+
+    public function getDuration() {
+        return $this->duration;
+    }
+
+    public function getMysqliData() {
+        return $this->mysqliData;
+    }
+
+    public function getGenre() {
+        return $this->genre;
     }
         
 }
